b6f6839caf43bdaac64e8055bfc983486369e0cb
Zachery DeLong <zacherypd@gmail.com>
2017-03-18 14:58:04 -0400

    Implementing basic association test for FeedCategory model.

--- tests/models/FeedCategory_test.js
+++ tests/models/FeedCategory_test.js
@@ -49,73 +49,39 @@ global.getTestDB = co.wrap(function* () {
 
 
 describe('FeedCategory', function() {
-  it('100 models', co.wrap(function* () {
-    for (let i = 1; i < 100; i++) {
-      let sequelize = yield getTestDB();
-      let models = sequelize.models;
-      let usr1 = yield models.User.create({
-        id: i,
-        login: 'sdfasd',
-        pwd_hash: 'sdfads'
-      });
-      let key1 = yield models.FeedCategory.create({
-        id: i,
-        title: 'Test Title',
-        owner_uid: i
-      });
-      let keys = yield models.FeedCategory.findAll({ attributes: ['id'] });
-      // console.log(keys);
-      let key = keys[0];
-      expect(key).to.have.property('id');
-      expect(key.id).to.equal(i);
-    }
-  }));
-  it('200 models', co.wrap(function* () {
-    for (let i = 1; i < 200; i++) {
-      let sequelize = yield getTestDB();
-      let models = sequelize.models;
-      let usr1 = yield models.User.create({
-        id: i,
-        login: 'sdfasd',
-        pwd_hash: 'sdfads'
-      });
-      let key1 = yield models.FeedCategory.create({
-        id: i,
-        title: 'Test Title',
-        owner_uid: i
-      });
-      let keys = yield models.FeedCategory.findAll({ attributes: ['id'] });
-      // console.log(keys);
-      let key = keys[0];
-      expect(key).to.have.property('id');
-      expect(key.id).to.equal(i);
-    }
+  it('a valid record should be inserted', co.wrap(function* () {
+    let sequelize = yield getTestDB();
+    let models = sequelize.models;
+    let usr1 = yield models.User.create({
+      id: 1,
+      login: 'sdfasd',
+      pwd_hash: 'sdfads'
+    });
+    let key1 = yield models.FeedCategory.create({
+      id: 1,
+      title: 'Test Title',
+      owner_uid: 1
+    });
+    let keys = yield models.FeedCategory.findAll({ attributes: ['id'] });
+    // console.log(keys);
+    let key = keys[0];
+    expect(key).to.have.property('id');
+    expect(key.id).to.equal(1);
   }));
 
-  describe('validations', function() {
-    it('is invalid without a user', co.wrap(function* (done) {
+  describe('associations', function() {
+    it('has User property', co.wrap(function* (done){
       let sequelize = yield getTestDB();
-      let models = sequelize.models;
-      let db = getTestDB();
-
-      var sequelizeValidationErrorThrown = false;
-      try{
-        let key1 = yield models.FeedCategory.create({
-          id: 1,
-          title: 'Test Title'
-        });
-      } catch( e ) {
-        if ( e.name == "SequelizeValidationError" && e.message == "notNull Violation: owner_uid cannot be null")
-          sequelizeValidationErrorThrown = true;
-      }
-
-      expect(sequelizeValidationErrorThrown).to.equal(true);
+      console.log('debugging');
+      console.info(sequelize.models.FeedCategory.associations);
+      expect(sequelize.models.FeedCategory.associations).to.have.property('User');
     }));
+  });
 
+  describe('invalid states validation', function() {
     it('is invalid without a title', co.wrap(function* (done) {
       let sequelize = yield getTestDB();
       let models = sequelize.models;
-      let db = getTestDB();
 
       var sequelizeValidationErrorThrown = false;
       try{
